1285bb15fae65e6ceb238f74cd35980ba80ba759
Damian Zmuda <damian.zmuda@beefee.uk.co>
2019-12-09 12:57:50 +0100

    Remove static data from Aplication component state

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -4,39 +4,9 @@ import { firestore } from '../firebase';
 
 class Application extends Component {
   state = {
-    posts: [
-      {
-        id: '1',
-        title: 'A Very Hot Take',
-        content:
-          'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!',
-        user: {
-          uid: '123',
-          displayName: 'Bill Murray',
-          email: 'billmurray@mailinator.com',
-          photoURL: 'https://www.fillmurray.com/300/300',
-        },
-        stars: 1,
-        comments: 47,
-      },
-      {
-        id: '2',
-        title: 'The Sauciest of Opinions',
-        content:
-          'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!',
-        user: {
-          uid: '456',
-          displayName: 'Mill Burray',
-          email: 'notbillmurray@mailinator.com',
-          photoURL: 'https://www.fillmurray.com/400/400',
-        },
-        stars: 3,
-        comments: 0,
-      },
-    ],
+    posts: [],
   };
 
-  // ------------------------------------------------------
   async componentDidMount() {
     const snapshot = await firestore.collection('posts').get();
 
@@ -52,7 +22,6 @@ class Application extends Component {
       posts
     }))
   }
-  // ------------------------------------------------------
 
   handleCreate = post => {
     const { posts } = this.state;
