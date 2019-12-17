3b6e9401fe40ece48825e7b9e0822fbe0e48d7de
Damian Zmuda <damian.zmuda@beefee.uk.co>
2019-12-17 10:04:44 +0100

    Refactor userProfilePage component

--- src/components/UserProfilePage.jsx
+++ src/components/UserProfilePage.jsx
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+import { firestore, auth } from '../firebase';
 
 class UserProfilePage extends Component {
+
+    state = {
+        displayName: ''
+    }
+    imageInput = null;
+
+    get uid() {
+        return auth.currentUser.uid;
+    }
+
+    get userRef() {
+        return firestore.collection('users').doc(this.uid);
+    }
+
+    handleChange = event => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const { displayName } = this.state;
+        if (displayName) {
+            this.userRef.update({ displayName });
+        }
+    }
+
     render() {
+        const { displayName } = this.state;
+
         return (
-            <div>
-                I am the user profile page.
-            </div>
+            <section className="UserProfile">
+                <form onSubmit={this.handleSubmit} className="UpdateUser">
+                    <input
+                        type="text"
+                        name="displayName"
+                        value={displayName}
+                        placeholder="Display Name"
+                        onChange={this.handleChange}
+                    />
+                    <input type="file" ref={ref => (this.imageInput = ref)} />
+                    <input className="update" type="submit" />
+                </form>
+            </section>
         );
     }
 }
